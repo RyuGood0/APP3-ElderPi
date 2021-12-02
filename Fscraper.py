30029230674e76ec31e1ced49eebee6d928fbefe
Mytricker0 <patrick.preda3@gmail.com>
2021-12-02 21:45:32 +0100

    added a script to parse a text document

---
+++ Fscraper.py
@@ -0,0 +1,18 @@
+def get_counties():
+    file = open('list_of_countries.txt').read()
+    
+    temp = []
+    
+    for line in file.split('\n'):
+        line = "".join(word+' ' for word in line.split() if word.lower() not in ['\n', '\r','drapeau','du','de',"l'","la","le"])
+        line = line[2:] if line[0:2] == "l'" else line
+        line = line.replace(',','.')
+
+        
+        sline = line.split()
+        temp.append(sline)
+    return temp
+        
+def remove_duplicates(list):
+    return list[:1] + [item for item in list[1:] if item not in list[:1]]
+print(remove_duplicates(get_counties()))
