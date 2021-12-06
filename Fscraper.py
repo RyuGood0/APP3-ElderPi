d94db7fde9207001b087cb432d6b24be92c719d2
mytricker0 <patrick.preda3@gmail.com>
2021-12-06 12:39:02 +0100

    trying to see if git is working on my PC

--- Fscraper.py
+++ Fscraper.py
@@ -1,3 +1,4 @@
+import copy
 def get_counties():
     file = open('list_of_countries.txt').read()
     
@@ -13,7 +14,41 @@ def get_counties():
         temp.append(sline)
     return temp
         
-def remove_duplicates(list):
-    return list[:1] + [item for item in list[1:] if item not in list[:1]]
-print(remove_duplicates(get_counties()))
-#
+def remove_duplicates(values):
+    names = copy.deepcopy(values)
+    for i in range(len(names)):
+        temp = []
+        for j in range(len(names[i])-8):
+            if names[i][j] not in temp:
+                temp.append(values[i][j])
+        names[i] = temp
+    
+    
+    for i in range(len(values)):
+        
+        temp = []
+        for j in range(len(values[i])-8,len(values[i])):
+            
+            temp.append(values[i][j])
+        values[i] = temp
+    
+    for item in range(len(names)):
+        
+        if len(names[item]) != 1:
+            names[item] = ["".join(element+ ' ' for element in names[item])]
+            
+            
+    return [names, values]
+
+
+def list_to_dict(both_lists):
+    combined_dict = {}
+
+    names = both_lists[0]
+    values = both_lists[1]
+
+    for name,value in zip(names,values):
+        combined_dict[''.join(name)] = value
+    open('countries_dict.txt','w').write(str(combined_dict))
+    return combined_dict
+
