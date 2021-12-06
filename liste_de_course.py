b8655d3646549566c9936a436159e617808002ef
RyuGood0 <ryugood0@gmail.com>
2021-12-06 12:04:48 +0100

    Commencement des fonctionalités de base

---
+++ liste_de_course.py
@@ -0,0 +1,25 @@
+def créer_liste_de_course():
+    filepath = "liste_de_course.txt"
+    with open(filepath, "w") as f:
+        f.write("")
+
+def ajouter_produit(produit):
+    filepath = "liste_de_course.txt"
+    with open(filepath, "a") as f:
+        f.write(produit + "\n")
+
+def lister_produits():
+    filepath = "liste_de_course.txt"
+    product_list = []
+    with open(filepath, "r") as f:
+        for line in f:
+            product_list.append(line.strip())
+
+def supprimer_produit(produit):
+    filepath = "liste_de_course.txt"
+    with open(filepath, "r") as f:
+        lines = f.readlines()
+    with open(filepath, "w") as f:
+        for line in lines:
+            if line.strip() != produit:
+                f.write(line)
