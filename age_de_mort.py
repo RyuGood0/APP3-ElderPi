f19628c43ee9ec99f65e7bc1178029a4bb41e244
mytricker0 <patrick.preda3@gmail.com>
2021-12-12 14:09:04 +0100

    chagements de l'age_de_mort.py

--- age_de_mort.py
+++ age_de_mort.py
@@ -1,6 +1,6 @@
 import json
-# import rhasspy
-# from sense_hat import SenseHat
+import rhasspy
+from sense_hat import SenseHat
 
 
 
@@ -19,17 +19,12 @@ def age_de_mort(dic):
     age = float(dic['age'])
     pays = dic['pays']
 
-    #if age > 87 : rhasspy.text_to_speech("Vous êtes déjà censés être morts .")
 
     difference_age = float(raw_dict[pays][sexe]) - age
     difference_age = round(difference_age, 2)
     
-    if difference_age < 0 : print("Vous êtes déjà censés être morts .")
-    #rhasspy.text_to_speech("Vous avez " + str(difference_age) + " ans avant de mourir.")
+    if difference_age < 0 : rhasspy.text_to_speech("Vous êtes déjà censés être morts .")
     
-    else : print("Vous avez " + str(difference_age) + " ans avant de mourir.")
-    #rhasspy.text_to_speech("Votre pays est numéro" + str(raw_dict[pays][sexe-1]) + 'mondial ')
+    else : rhasspy.text_to_speech("Vous avez " + str(difference_age) + " ans avant de mourir.")
 
-    print("Votre pays est numéro " + str(raw_dict[pays][sexe-1]) + ' mondial ')
-
-age_de_mort({'sexe': 'une femme', 'age' : '69', 'pays' : 'niger'})
+    rhasspy.text_to_speech("Votre pays est numéro " + str(raw_dict[pays][sexe-1]) + ' mondial ')
