be91f4b767ed752d6deaad46b908dae9474ad8d2
mytricker0 <patrick.preda3@gmail.com>
2021-12-12 18:03:42 +0100

    main avancé

--- temp.py
+++ temp.py
@@ -1,6 +1,9 @@
 import rhasspy
 from sense_hat import SenseHat
 from mot_de_passe import *
+from age_de_mort import age_de_mort
+from playmusic import *
+from liste_de_course import *
 sense = SenseHat()
 
 # Lance l'apprentissage du fichier sentences.ini. Commentez cette partie si vous souhaitez ne pas le lancer 
@@ -28,20 +31,47 @@ while True:
 		elif started_session == True:
 			if saved_password == None and is_mdp():
 				if intention == "EnregistrerMDP":
-					saved_password = intent["variables"]
+					if verifier_mdp(''.join(arg for arg in ntent["variables"].values())):
+						saved_password = intent["variables"]
+						rhasspy.text_to_speech("Mot de passe enregistré, vous pouvez commencer.")
+						print("Vous pouvez commencer")
+					else:
+						rhasspy.text_to_speech("Mot de passe invalide")
+						print("Mot de passe invalide")
 			elif saved_password == None and not is_mdp():
 				if intention == "NouveauMDP":
 					creer_mdp(intent["variables"])
-			else:					
+					saved_password = intent["variables"]
+					rhasspy.text_to_speech("Mot de passe enregistré, vous pouvez commencer.")
+					print("Vous pouvez commencer")
+			else:
 				if intention == "NouveauCode":
 					enregistrer_code(intent["variables"], saved_password)
 				elif intention == "LireCode":
 					lire_code(intent["variables"], saved_password)
+				elif intention == "AgeDeMort":
+					age_de_mort(intent["variables"])
+				elif intention == "AjoutListe":
+					objet = intent["variables"]["objet"]
+					ajouter_produit(objet)
+					rhasspy.text_to_speech(f"{objet} ajouté")
+				elif intention == "LireListe":
+					rhasspy.text_to_speech(''.join(objet + " " for objet in lister_produits()))
+				elif intention == "SupprObjet":
+					objet = intent["variables"]["objet"]
+					supprimer_produit(objet)
+					rhasspy.text_to_speech(f"{objet} supprimé")
+				elif intention == "JouerMusique":
+					playmusic()
 			if intention == "Fin":
+				saved_password = None
 				started_session = False
+				rhasspy.text_to_speech("Fin de session, au revoir")
 
 	except Exception as e:
 		if e == KeyboardInterrupt:
+			rhasspy.text_to_speech("Au revoir et a bientot.")
 			break
 		else:
 			print("La phrase n'a pas été")
+			print(e)
